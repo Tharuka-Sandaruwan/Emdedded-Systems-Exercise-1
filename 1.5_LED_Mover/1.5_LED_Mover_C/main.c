ea6a1d9f661aa6ab837561dfa5ebdddb1c4ab370
Tharuka Sandaruwan <ts2000614@gmail.com>
2022-12-11 09:51:46 +0530

    initialized the c code

---
+++ 1.5_LED_Mover/1.5_LED_Mover_C/main.c
@@ -0,0 +1,19 @@
+/*
+ * 1.5_LED_Mover_C.c
+ *
+ * Created: 12/11/2022 9:49:29 AM
+ * Author : Tharuka Sandaruwan
+ */ 
+
+#include <avr/io.h>
+#define F_CPU 16000000UL
+#include<util/delay.h>
+
+int main(void)
+{
+    
+    while (1) 
+    {
+    }
+}
+
